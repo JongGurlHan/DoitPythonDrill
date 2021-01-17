dbb7ee37d162aca89d713a92fe41a7a03d8b6526
JongGurlHan <gojkhan13@gmail.com>
2021-01-17 21:44:55 +0900

    1.17 drill

--- part4/Test5.py
+++ part4/Test5.py
@@ -5,3 +5,10 @@ f1.close()
 
 f2 = open("test.txt", 'r')
 print(f2.read())
+f2.close()
+
+with open("test.txt", 'a') as f1:
+    f1.write(" this is test sentence")
+
+with open("test.txt" , 'r') as f2:
+    print(f2.read())
