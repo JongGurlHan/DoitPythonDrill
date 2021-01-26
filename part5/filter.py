5b5dbc99d253e7731dc5d4df2431883c33e40621
JG HAN <60098769+JongGurlHan@users.noreply.github.com>
2021-01-26 22:31:32 +0900

    1.26  내장함수 학습

---
+++ part5/filter.py
@@ -0,0 +1,12 @@
+def positive(l):
+    result = []
+    for i in l:
+        if i > 0:
+            result.append(i)
+    return result
+
+print(positive([1,-3,2,0,-5,6]))
+
+def positivE(x):
+    return x > 0
+
